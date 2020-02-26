e94ad7e03862f3f255a0562499c07d1cb95b21ba
Johanna Devaney <jcdevaney@gmail.com>
2020-02-26 18:42:54 -0500

    class 4

---
+++ class4/importData.py
@@ -0,0 +1,12 @@
+import os
+import tarfile
+from six.moves import urllib
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH): 
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+        tgz_path = os.path.join(housing_path, "housing.tgz")
+        urllib.request.urlretrieve(housing_url, tgz_path)
+        housing_tgz = tarfile.open(tgz_path)
+        housing_tgz.extractall(path=housing_path)
+        housing_tgz.close()
